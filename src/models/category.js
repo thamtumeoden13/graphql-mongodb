d63c60c678b5db2123a6cd57218f98e19fd7388b
cnblue13 <vu.lehoang>
2021-11-18 23:23:00 +0700

    change defind

--- src/models/category.js
+++ src/models/category.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const { PosterType, CategoryType } = require('../typeDefs')
+
+const {
+    GraphQLID,
+    GraphQLBoolean,
+    GraphQLString,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLNonNull
+} = graphql
+
+
 const categorySchema = new Schema({
     name: String,
     tile: String,
     content: String,
-    posters: [{ type: Schema.Types.ObjectId, ref: 'Poster' }]
+    posters: { type: new GraphQLList(PosterType) },
+    // posters: [{ type: Schema.Types.ObjectId, ref: 'Poster' }]
 })
 module.exports = mongoose.model('Category', categorySchema)
--- src/models/poster.js
+++ src/models/poster.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const { PosterType, CategoryType } = require('../typeDefs')
+
+const {
+    GraphQLID,
+    GraphQLBoolean,
+    GraphQLString,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLNonNull
+} = graphql
+
 const posterSchema = new Schema({
     name: String,
     tile: String,
     content: String,
-    categoryId: { type: Schema.Types.ObjectId, ref: 'Category' },
+    // categoryId: { type: Schema.Types.ObjectId, ref: 'Category' },
+    posters: { type: new GraphQLObjectType(CategoryType) },
 })
 module.exports = mongoose.model('Poster', posterSchema)
